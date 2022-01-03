6f3439b0b2b037fc8d600ded0a8b074e50c079e6
12345120 <hehe9343@gmail.com>
2022-01-03 15:01:22 -0500

    added Tween from GSAP, made change in css for ScrollMagic

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-  triggerElement: ".card",
-  duration: 100,
-  triggerHook: 0,
+  triggerElement: ".body",
+  // duration: 30,
+  triggerHook: 0.5,
 })
   .setClassToggle(".card", "show")
   .addIndicators()
   .addTo(controller);
+
